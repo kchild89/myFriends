ccedd98cd26885eff5ea77b996fc2da53a2a6c17
Kevin Child <kchild89@gmail.com>
2025-01-07 09:36:03 -0700

    add friends

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ import express from "express";
 
 const app = express();
 const port = process.env.PORT || 3000;
+const friends = [];
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -9,7 +10,19 @@ app.use(express.static("public"));
 
 app.get("/getCurrentTime", (req, res) => {
   const date = new Date();
-  res.json({ currentDate: date });
+  res.json({ currentDate: date.toLocaleString() });
+});
+
+app.post("/addFriend", (req, res) => {
+  const name = req.body.name;
+  const email = req.body.email;
+  const phone = req.body.phone;
+  const age = req.body.age;
+
+  const friend = { name, email, phone, age };
+  friends.push(friend);
+
+  res.json({ success: true, friend: friend });
 });
 
 app.listen(port, () => {
